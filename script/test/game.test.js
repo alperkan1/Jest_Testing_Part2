83cc819461c6532f6ed21e01073f2cdc0d6328f9
alperkan1 <seanhack@gmail.com>
2021-11-22 14:25:28 +0000

    add

--- script/test/game.test.js
+++ script/test/game.test.js
@@ -17,5 +17,13 @@
      test("score key exists", () => {
          expect("score" in game).toBe(true);
      });
-     
+     test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1","button2","button3","button4"]);
+    });
  });
